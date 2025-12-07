3af85a63c8f230effac288f26e56ea10cf0f758a
CrazyCat911 <127078477+CrazyCat911@users.noreply.github.com>
2025-12-07 17:45:42 +1000

    3a

---
+++ 3/a.py
@@ -0,0 +1,22 @@
+### INPUT PROCESSING ###
+with open("3/input.dat", "r") as f:
+    plaintext_input = f.read()
+
+formatted_input = [list(bank) for bank in plaintext_input.strip().split("\n")]
+
+### THE CODE ###
+def get_joltage(bank: list[str]) -> int:
+    first_digit = max(bank[:-1], key=lambda v: int(v))
+    first_digit_location = bank.index(first_digit)
+
+    next_digit = max(bank[first_digit_location + 1:])
+
+    return int(first_digit + next_digit)
+
+
+total_joltage = 0
+
+for bank in formatted_input:
+    total_joltage += get_joltage(bank)
+
+print(total_joltage)
