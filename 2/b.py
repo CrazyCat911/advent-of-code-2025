b1d9ce1a0e2dbcf32636e1e8293f62b827d43cd5
CrazyCat911 <127078477+CrazyCat911@users.noreply.github.com>
2025-12-07 17:13:14 +1000

    2b

---
+++ 2/b.py
@@ -0,0 +1,21 @@
+### INPUT PROCESSING ###
+with open("2/input.dat", "r") as f:
+    plaintext_input = f.read()
+
+formatted_input = [[int(v) for v in v.split("-")] for v in plaintext_input.strip().split(",")]
+
+### THE CODE ###
+
+def id_invalid(id):
+    number_as_str = str(number)
+
+    return number_as_str in (number_as_str + number_as_str)[1:-1]
+
+total_invalid = 0
+
+for id_range in formatted_input:
+    for number in range(id_range[0], id_range[1] + 1):
+        if id_invalid(number):
+            total_invalid += number
+
+print(total_invalid)
