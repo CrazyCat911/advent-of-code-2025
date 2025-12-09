702e8217754c0b7cdd7c836cc571f3c6ae86bfe6
Crazy_Cat911 <127078477+CrazyCat911@users.noreply.github.com>
2025-12-09 00:56:03 +0000

    6a

---
+++ 6/a.py
@@ -0,0 +1,22 @@
+### INPUT PROCESSING ###
+with open("6/input.dat", "r") as f:
+    plaintext_input = f.read()
+
+operators = [v for v in plaintext_input.strip().split("\n")[-1].split(" ") if v]
+numbers = [[int(v) for v in row.split(" ") if v] for row in plaintext_input.strip().split("\n")[:-1]]
+
+grand_total = 0
+
+for q, op in enumerate(operators):
+    question_answer = numbers[0][q]
+    
+    if op == "+":
+        for number in numbers[1:]:
+            question_answer += number[q]
+    elif op == "*":
+        for number in numbers[1:]:
+            question_answer *= number[q]
+    
+    grand_total += question_answer
+
+print(grand_total)
